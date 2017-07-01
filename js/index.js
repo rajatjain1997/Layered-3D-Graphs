43c810222bcb159b7a96152ab0d3ec523aba7150
Rohit <Rohit>
2017-07-02 00:16:37 +0530

    time complexity of graph layering algorithm reduced

--- js/index.js
+++ js/index.js
@@ -7,27 +7,74 @@ var simulation = d3_force.forceSimulation().numDimensions(3)
 d3.json("../data/names.json", function(error, graph) {
   if (error) throw error;
 
-  var i;
   var layers=1;
-  for(i=0;i<graph.links.length;i++)
-  {
-    if(graph.nodes[graph.links[i].source-1].fz>=graph.nodes[graph.links[i].target-1].fz)
+   var i;
+  // for(i=0;i<graph.links.length;i++)
+  // {
+  //   if(graph.nodes[graph.links[i].source-1].fz>=graph.nodes[graph.links[i].target-1].fz)
+  //     {
+  //       graph.nodes[graph.links[i].target-1].fz=graph.nodes[graph.links[i].source-1].fz+1;
+  //       if(graph.nodes[graph.links[i].target-1].fz>layers)
+  //       layers=graph.nodes[graph.links[i].target-1].fz;
+  //     }
+  //     for(j=0;j<graph.links.length;j++)
+  //     {
+  //       if(graph.nodes[graph.links[j].source-1].fz>=graph.nodes[graph.links[j].target-1].fz)
+  //         graph.nodes[graph.links[j].target-1].fz=graph.nodes[graph.links[j].source-1].fz+1;
+  //       if(graph.nodes[graph.links[j].target-1].fz>layers)
+  //       layers=graph.nodes[graph.links[j].target-1].fz;
+  //     }   
+  // }
+
+    var source = new Array();
+    var target = new Array();
+
+    for(i=0;i<graph.nodes.length;i++)
+    {
+      source[i] = new Array();
+      target[i] = new Array();
+    }
+
+    for(i=0;i<graph.links.length;i++)
+    {
+      source[graph.links[i].target-1].push(graph.links[i].source);
+      target[graph.links[i].source-1].push(graph.links[i].target);
+    }
+
+    var noOfChild = new Array();
+    var queue = new Array();
+
+    for(i=0;i<graph.nodes.length;i++)
+    {
+      noOfChild[i]=0;
+      if(source[i].length==0)
       {
-        graph.nodes[graph.links[i].target-1].fz=graph.nodes[graph.links[i].source-1].fz+1;
-        if(graph.nodes[graph.links[i].target-1].fz>layers)
-        layers=graph.nodes[graph.links[i].target-1].fz;
+        queue.push(graph.nodes[i].id);
       }
-      for(j=0;j<graph.links.length;j++)
+    } 
+
+    while(queue.length!=0)
+    {
+      var ele = queue.pop();
+      for(i=0;i<target[ele-1].length;i++)
       {
-        if(graph.nodes[graph.links[j].source-1].fz>=graph.nodes[graph.links[j].target-1].fz)
-          graph.nodes[graph.links[j].target-1].fz=graph.nodes[graph.links[j].source-1].fz+1;
-        if(graph.nodes[graph.links[j].target-1].fz>layers)
-        layers=graph.nodes[graph.links[j].target-1].fz;
-      }   
-  }
+        noOfChild[target[ele-1][i]-1]++;
+        if(graph.nodes[target[ele-1][i]-1].fz<(graph.nodes[ele-1].fz+1))
+        graph.nodes[target[ele-1][i]-1].fz=graph.nodes[ele-1].fz+1;
+        if(graph.nodes[target[ele-1][i]-1].fz>layers)
+            {
+              layers=graph.nodes[target[ele-1][i]-1].fz;
+            }
+
+        if(noOfChild[target[ele-1][i]-1]==source[target[ele-1][i]-1].length)
+        {
+          queue.push(target[ele-1][i]);
+        }
+      }
+    }
 
   console.log(layers);
-
+  
   for(var iter = 1; iter<=layers; iter++) {
     (function(iter) {
       simulation = simulation.force("layerrepel" + iter, isolate(d3.forceManyBody().strength(-100), graph.nodes, function(d) {return d.fz == iter;}));
