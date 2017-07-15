18a466d5fdd305cb62284a0dc5696115884e5f1f
Rajat Jain <1997.rajatjain@gmail.com>
2017-07-15 14:15:01 +0530

    Added redis to reduce memory bottleneck + More configuration options

--- js/index.js
+++ js/index.js
@@ -5,64 +5,97 @@ const fs = require('fs');
 const app = express();
 const neo4j = require('neo4j-driver').v1;
 const config = require('nodejs-config') (path.resolve("../"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-const port = 80;
+const kue = require('kue');
+
+const port = config.get('server').port;
+
+//Defining Neo4J connections
 
 const driver = neo4j.driver(config.get('neo4j').bolt, neo4j.auth.basic(config.get('neo4j').username, config.get('neo4j').password));
-const session = driver.session();
 var nodesprocessed = 0;
 var edgesprocessed = 0;
 
+//Defining Express server
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("../"));
 app.use('/',express.static("../views"));
 app.engine('.html', require('ejs').__express);
 app.set('views',  path.resolve("../html"));
 app.set('view engine', 'html');
 
+//Defining Redis Kue
+
+kue.app.listen(config.get('server').kue);
+
+var neoQueue = kue.createQueue();
+
+neoQueue.process('neo4j', function(job, done) {
+	var session = driver.session();
+	session.run(job.data.request, job.data.params)
+		.then(r => {
+			session.close();
+			done();
+		})
+		.catch(err => {
+			session.close();
+			done(err);
+		});
+});
+
 app.get('/', function(req, res) {
+	var data = "meta-academy";
+	if(req.query.data) {
+		data = req.query.data;
+	}
 	res.render('index', {
 		port: port,
-		data: req.query.data
+		data: data
 	});
 });
 
-app.post('/', function(req, res) {
-  var data = req.body.data;
-  fs.writeFile('../tmp/test.json', data, "utf-8", 4,function(err) {
-  	if(err) {	
-  		res.send(err);
-  	}
-  	res.send("Serialized!");
-  });
-});
-
 app.post('/neo4j/reset', function(req, res) {
-	var result = session.run("Match (n) detach delete n");
-	result.then(r => {res.send("Database Cleared!");});
+	neoQueue.create('neo4j', {
+		driver: driver.session,
+		request: "Match (n) detach delete n",
+		params: {}
+	}).save();
+	res.send("Job queued!");
 	nodesprocessed = 0;
 	edgesprocessed = 0;
 });
 
 app.post('/neo4j/node', function(req, res) {
 	var node = req.body.node;
-	var result = session.run("CREATE (n:Node {x: $x, y:$y, z:$fz, id: $id, index: $index, name: $name})", node);
-	result.then(r=> {res.send("Node added!");});
+	neoQueue.create('neo4j', {
+		driver: driver.session,
+		request: "CREATE (n:Node {x: $x, y:$y, z:$fz, id: $id, index: $index, name: $name})",
+		params: node
+	}).save();
+	res.send("Job queued!");
 	nodesprocessed++;
 });
 
 app.post('/neo4j/edge', function(req, res) {
 	var source = req.body.edge.source.id;
 	var target = req.body.edge.target.id;
-	var result = session.run("Match (m:Node {id: $source}), (n:Node {id: $target}) create (m)-[r:pre]->(n)", {"source": source, "target": target});
-	result.then(r=> {res.send("Edge added!");});
+	neoQueue.create('neo4j', {
+		driver: driver.session,
+		request: "Match (m:Node {id: $source}), (n:Node {id: $target}) create (m)-[r:pre]->(n)",
+		params: {"source": source, "target": target}
+	}).save();
+	res.send("Job queued!");
 	edgesprocessed++;
 });
 
 app.post('/neo4j/index', function(req, res) {
-	var result = session.run("CREATE INDEX ON :Node(id)");
-	result.then(r=> {res.send("Index created!");});
+	neoQueue.create('neo4j', {
+		driver: driver.session,
+		request: "CREATE INDEX ON :Node(id)",
+		params: {}
+	}).save();
+	res.send("Job queued!");
 })
 
 app.get('/neo4j', function(req, res) {
