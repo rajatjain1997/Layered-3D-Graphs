27ee1407939c039aebb2f531ed176d879820ead4
RohitG28 <rohit.ghivdonde@gmail.com>
2017-08-14 15:45:06 +0530

    Added drop down list in place of search in the iframe UI

--- R/plotlyShiny.R
+++ R/plotlyShiny.R
@@ -30,8 +30,9 @@ display <- ""
 action <- 0
 
 ui <- fluidPage(
-  fluidRow(column(4),column(4,textInput("search", "Search", "")),column(4)),
-  fluidRow(column(4),column(4,actionButton("enter", "Go!")),column(4)),
+  # fluidRow(column(4),column(4,textInput("search", "Search", "")),column(4)),
+  fluidRow(column(4),column(4,selectInput("search","",choices=nodes.text)),column(4)),
+  fluidRow(column(4),column(4,actionButton("enter", "Search")),column(4)),
   fluidRow(column(6,plotlyOutput("plot",height = "500px")),
   column(6,plotlyOutput("click",height="500px")))
 )
--- shiny/sample-apps/rohit/ui.R
+++ shiny/sample-apps/rohit/ui.R
@@ -1,10 +1,14 @@
 library(plotly)
 library(shiny)
 
+neo4j.json <- rjson::fromJSON(file = "./../../../config/neo4j.json")
+  graph=RNeo4j::startGraph(paste(substr(neo4j.json$bolt,8,nchar(neo4j.json$bolt)),":7474/db/data", sep=""), username=neo4j.json$username, password=neo4j.json$password)
+
+nodes.text=(unlist(RNeo4j::getNodes(graph,"MATCH (p:Node) RETURN p.name")))
 
 fluidPage(
-  fluidRow(column(4),column(4,textInput("search", "Search", "")),column(4)),
-  fluidRow(column(4),column(4,actionButton("enter", "Go!")),column(4)),
+  fluidRow(column(12,align = "center",selectInput("search","",choices=nodes.text), actionButton("enter", "Search"))),
   fluidRow(column(6,plotlyOutput("plot",height = "500px")),
-  column(6,plotlyOutput("click",height="500px")))
+  column(6,plotlyOutput("click",height="500px"))),
+  fluidRow(column(12, align = "center", p("Click on any node to search for its subgraph! Alternatively, you can use the search bar above.")))
 )
